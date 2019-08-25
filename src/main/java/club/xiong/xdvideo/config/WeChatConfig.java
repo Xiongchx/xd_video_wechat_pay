d2977c3e42adf9e57c97647706750ae209147493
xiongchx <loveryuchi@vip.qq.com>
2019-08-26 00:26:09 +0800

    增加配置项获取方法

--- src/main/java/club/xiong/xdvideo/config/WeChatConfig.java
+++ src/main/java/club/xiong/xdvideo/config/WeChatConfig.java
@@ -42,6 +42,73 @@ public class WeChatConfig {
     @Value("${wxpay.appsecret}")
     private String appsecret;
 
+    /**
+     * 开放平台appid
+     * @author 26917
+     * @date 2019/8/25 17:36 
+     * @param null
+     * @return 
+     */
+    @Value("${wxopen.appid}")
+    private String openAppid;
+
+    /**
+     * 开放平台appsecret
+     * @author 26917
+     * @date 2019/8/25 17:36 
+     * @param null
+     * @return 
+     */
+    @Value("${wxopen.appsecret}")
+    private String openAppsecret;
+
+    /**
+     * 开放平台回调url
+     * @author 26917
+     * @date 2019/8/25 17:37 
+     * @param null
+     * @return 
+     */
+    @Value("${wxopen.redirect_url}")
+    private String openRedirectUrl;
+
+    /**
+     * 开放平台二维码连接
+     * @author 26917
+     * @date 2019/8/25 17:37 
+     * @param null
+     * @return 
+     */
+    private final static String OPEN_QRCODE_URL= "https://open.weixin.qq.com/connect/qrconnect?appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_login&state=%s#wechat_redirect";
+
+    public static String getOpenQrcodeUrl() {
+        return OPEN_QRCODE_URL;
+    }
+
+    public String getOpenAppid() {
+        return openAppid;
+    }
+
+    public void setOpenAppid(String openAppid) {
+        this.openAppid = openAppid;
+    }
+
+    public String getOpenAppsecret() {
+        return openAppsecret;
+    }
+
+    public void setOpenAppsecret(String openAppsecret) {
+        this.openAppsecret = openAppsecret;
+    }
+
+    public String getOpenRedirectUrl() {
+        return openRedirectUrl;
+    }
+
+    public void setOpenRedirectUrl(String openRedirectUrl) {
+        this.openRedirectUrl = openRedirectUrl;
+    }
+
     public String getAppId() {
         return appId;
     }
